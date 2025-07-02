e76740192715f1b2c7a4243f54a8418846528701
Kinsasha <kennedyokechukwu203@gmail.com>
2025-07-02 08:52:08 +0100

    i started the construction of my calculated completed the design and styling about to begin javascript part

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(numberOne, numberTwo) {
+  numberOne + numberTwo;
+}
+function substract(numberOne, numberTwo) {
+  numberOne - numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  numberOne * numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  numberOne / numberTwo;
+}
